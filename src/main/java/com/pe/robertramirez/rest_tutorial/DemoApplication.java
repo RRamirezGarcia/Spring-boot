3fe14f53210c660084c236fb92b219a9ab6ad614
Robert Ramirez <robert.ramirez.garcia@gmail.com>
2020-03-07 21:03:18 -0500

    Se sube código aplicación básica de Spring-boot + MongoDB

---
+++ src/main/java/com/pe/robertramirez/rest_tutorial/DemoApplication.java
@@ -0,0 +1,13 @@
+package com.pe.robertramirez.rest_tutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pe/robertramirez/rest_tutorial/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pe.robertramirez.rest_tutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
